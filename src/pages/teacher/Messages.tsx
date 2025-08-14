e41499d61f8a9afe4a3648e74e9937c83deb9774
MarcAlmeidaMelo <almeida.marcel.melo@gmail.com>
2025-08-14 03:36:49 -0300

    Update Messages.tsx
    removi as funções que estavam na pagina messages

--- src/pages/teacher/Messages.tsx
+++ src/pages/teacher/Messages.tsx
@@ -1,10 +1,1 @@
 import AppLayout from "@/layouts/AppLayout";
-import SimpleChat from "@/components/chat/SimpleChat";
-
-export default function TeacherMessages() {
-  return (
-    <AppLayout role="teacher" title="Mensagens" description="Envie comunicados aos pais">
-      <SimpleChat youLabel="Você" otherLabel="Pais" />
-    </AppLayout>
-  );
-}
